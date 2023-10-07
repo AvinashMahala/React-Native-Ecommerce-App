a68d8e5281e233bf0230c5484e7b7301168a1323
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-10-06 19:14:22 -0500

    update

--- api/index.js
+++ api/index.js
@@ -6,27 +6,111 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 8000;
-const cors=require("cors");
+const cors = require("cors");
 app.use(cors());
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const jwt = require("jsonwebtoken");
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.zvwn3yq.mongodb.net/",{
+mongoose
+  .connect("mongodb+srv://admin:admin@cluster0.zvwn3yq.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
+  })
+  .then(() => {
     console.log("Connected To MongooseDB!");
-}).catch((err)=>{
-    console.log("Error Connecting To MongoDB! ",err);
+  })
+  .catch((err) => {
+    console.log("Error Connecting To MongoDB! ", err);
+  });
+
+app.listen(port, () => {
+  console.log("Server is running on port 8000!");
 });
 
+const User = require("./models/user");
+const Order = require("./models/order");
 
-app.listen(port, ()=>{
-    console.log("Server is running on port 8000!");
-});
+//function to send Verification Email to the user
+const sendVerificationEmail = async (email, verificationToken) => {
+  //create a nodemailer transport
+
+  const transporter = nodemailer.createTransport({
+    //configure the email service
+    service: "gmail",
+    auth: {
+      user: "avinash.mahala.am@gmail.com",
+      pass: "ptfmtzengtvtzvfu",
+    },
+  });
+
+  //compose the email message.
+  const mailOptions = {
+    from: "amazon.com",
+    to: email,
+    subject: "Email Verification",
+    text: `Please click the following link to verify your email : http://localhost:8080/verify/${verificationToken}`,
+  };
+  //Send Email.
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("Error Sending Verification Email.", error);
+  }
+};
 
 //endpoint to register in the app.
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //Check if the email is already registered.
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return req.status(400).json({ message: "Email already registered." });
+    }
+
+    //Create a New User
+    const newUser = new User({ name, email, password });
+
+    //Generate and store the verification token
+    newUser.verificationToken = crypto.randomBytes(64).toString("hex");
+
+    //Save the user to the database.
+    await newUser.save();
+
+    //Send the verification email.
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+  } catch (error) {
+    console.log("Error Registering User: ", error);
+    res.status(500).json({ message: "Registration Failed." });
+  }
+});
+
+
+
+//Endpoint to verify the email.
+app.get("/verify/:token", async(req, res)=>{
+    try {
+        const token = req.params.token;
+        
+        //Find the user with the given verification token.
+        const user = await User.findOne({verificationToken:token});
+        if(!user){
+            return res.status(404).json({message:"Invalid Verification Token."})
+        }
+
+        //Mark.User as Verified.
+        user.verified = true;
+        user.verificationToken = undefined;
+
+        await user.save();
+
+        res.status(200).json({message:"Email Verified Successfully."});
+
+    } catch (error) {
+        res.status(500).json({message: "Email Verification Failed."});
+    }
+})
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -12,186 +12,222 @@ import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
+import axios from "axios";
+import { Alert } from "react-native";
 
 const RegisterScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
-    const navigation=useNavigation()
+  const navigation = useNavigation();
+  const handleRegister = () => {
+    // Check if any of the fields is empty
+    if (!name || !email || !password) {
+      Alert.alert("Validation Error", "Please fill in all the fields.");
+      return; // Don't proceed with the registration if any field is empty
+    }
+
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    //Send a Post request to the backend API.
+    axios
+      .post("http://localhost:8000/register", user)
+      .then((response) => {
+        console.log(response);
+        Alert.alert(
+          "Registration Successful!",
+          "You Have Registered Successfully."
+        );
+        setName("");
+        setPassword("");
+        setEmail("");
+      })
+      .catch((error) => {
+        console.log(error);
+        Alert.alert("Registration Failed.", "An Error Occured.");
+      });
+  };
+
   return (
     <SafeAreaView
-    style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
-  >
-    <View>
-      <Image
-        style={{ width: 150, height: 100, marginTop: 30 }}
-        source={{
-          uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png",
-        }}
-      />
-    </View>
-    <KeyboardAvoidingView>
-      <View style={{ alignItems: "center" }}>
-        <Text style={{ fontSize: 24, fontWeight: "bold", marginTop: 20 }}>
-          Register To Your Account
-        </Text>
-      </View>
-
-      <View style={{ marginTop: 70 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 5,
-            backgroundColor: "#D0D0D0",
-            paddingVertical: 5,
-            borderRadius: 5,
-            marginTop: 30,
-          }}
-        >
-          <Ionicons
-            style={{ marginLeft: 8 }}
-            name="ios-person"
-            size={24}
-            color="gray"
-          />
-          <TextInput
-            value={name}
-            onChangeText={(text) => setName(text)}
-            style={{
-              color: "gray",
-              marginVertical: 10,
-              width: 300,
-              fontSize: name ? 16 : 16,
-            }}
-            placeholder="Enter your Name "
-          />
-        </View>
-      </View>
-
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
       <View>
+        <Image
+          style={{ width: 150, height: 100, marginTop: 30 }}
+          source={{
+            uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png",
+          }}
+        />
+      </View>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 24, fontWeight: "bold", marginTop: 20 }}>
+            Register To Your Account
+          </Text>
+        </View>
+
+        <View style={{ marginTop: 70 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <Ionicons
+              style={{ marginLeft: 8 }}
+              name="ios-person"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              value={name}
+              onChangeText={(text) => setName(text)}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: name ? 16 : 16,
+              }}
+              placeholder="Enter your Name "
+            />
+          </View>
+        </View>
+
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 16 : 16,
+              }}
+              placeholder="Enter your Email "
+            />
+          </View>
+        </View>
+
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <AntDesign
+              style={{ marginLeft: 8 }}
+              name="lock1"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: password ? 16 : 16,
+              }}
+              placeholder="Enter your Password "
+            />
+          </View>
+        </View>
+
         <View
           style={{
+            marginTop: 12,
             flexDirection: "row",
             alignItems: "center",
-            gap: 5,
-            backgroundColor: "#D0D0D0",
-            paddingVertical: 5,
-            borderRadius: 5,
-            marginTop: 30,
+            justifyContent: "space-between",
           }}
         >
-          <MaterialIcons
-            style={{ marginLeft: 8 }}
-            name="email"
-            size={24}
-            color="gray"
-          />
-          <TextInput
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            style={{
-              color: "gray",
-              marginVertical: 10,
-              width: 300,
-              fontSize: email ? 16 : 16,
-            }}
-            placeholder="Enter your Email "
-          />
+          <Text>Keep Me Logged In</Text>
+          <Text style={{ color: "#007FFF", fontWeight: "500" }}>
+            Forgot Password
+          </Text>
         </View>
-      </View>
 
-      <View>
-        <View
+        <View style={{ marginTop: 80 }} />
+
+        <Pressable
+          onPress={handleRegister}
           style={{
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 5,
-            backgroundColor: "#D0D0D0",
-            paddingVertical: 5,
-            borderRadius: 5,
-            marginTop: 30,
+            width: 200,
+            backgroundColor: "#FEBE10",
+            borderRadius: 6,
+            marginLeft: "auto",
+            marginRight: "auto",
+            padding: 15,
           }}
         >
-          <AntDesign
-            style={{ marginLeft: 8 }}
-            name="lock1"
-            size={24}
-            color="gray"
-          />
-          <TextInput
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            secureTextEntry={true}
+          <Text
             style={{
-              color: "gray",
-              marginVertical: 10,
-              width: 300,
-              fontSize: password ? 16 : 16,
+              textAlign: "center",
+              color: "white",
+              fontSize: 16,
+              fontWeight: "bold",
             }}
-            placeholder="Enter your Password "
-          />
-        </View>
-      </View>
+          >
+            Register
+          </Text>
+        </Pressable>
 
-      <View
-        style={{
-          marginTop: 12,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text>Keep Me Logged In</Text>
-        <Text style={{ color: "#007FFF", fontWeight: "500" }}>
-          Forgot Password
-        </Text>
-      </View>
-
-      <View style={{ marginTop: 80 }} />
-
-      <Pressable
-        style={{
-          width: 200,
-          backgroundColor: "#FEBE10",
-          borderRadius: 6,
-          marginLeft: "auto",
-          marginRight: "auto",
-          padding: 15,
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "center",
-            color: "white",
-            fontSize: 16,
-            fontWeight: "bold",
-          }}
+        <Pressable
+          onPress={() => navigation.navigate("Login")}
+          style={{ textAlign: "center", color: "gray", fontSize: 16 }}
         >
-          Register
-        </Text>
-      </Pressable>
+          <Text
+            style={{
+              textAlign: "center",
+              color: "gray",
+              fontSize: 16,
+              marginTop: 20,
+            }}
+          >
+            Already Have An Account? Sign In
+          </Text>
+        </Pressable>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
 
-      <Pressable 
-          onPress={()=>navigation.navigate("Login")}
-      style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
-        <Text
-          style={{
-            textAlign: "center",
-            color: "gray",
-            fontSize: 16,
-            marginTop: 20,
-          }}
-        >
-          Already Have An Account? Sign In
-        </Text>
-      </Pressable>
-    </KeyboardAvoidingView>
-  </SafeAreaView>
-  )
-}
+export default RegisterScreen;
 
-export default RegisterScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
